2f2974a6cb6d038860750676007739b5c653c32b
Irshad Ahmad <irshad@wpsocio.com>
2023-02-11 20:57:56 +0530

    Add snapshot script

---
+++ bin/snapshots.php
@@ -0,0 +1,29 @@
+<?php
+
+if ( file_exists( __DIR__ . '/../vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/../vendor/autoload.php';
+}
+
+require_once __DIR__ . '/../tests/Utils.php';
+
+use WPTelegram\FormatText\Tests\Utils;
+use WPTelegram\FormatText\HtmlConverter;
+
+$files = glob( dirname( __DIR__ ) . '/tests/data/input/*.html' );
+
+foreach ( $files as $file ) {
+	$input = file_get_contents( $file );
+
+	foreach ( Utils::FORMATS as $format ) {
+
+		$output_path = Utils::getTestOutputPath( $file, $format );
+
+		$expected = file_get_contents( $output_path );
+
+		$converter = new HtmlConverter( [ 'format_to' => $format ] );
+
+		$output = $converter->convert( $input );
+
+		file_put_contents( $output_path, $output );
+	}
+}
